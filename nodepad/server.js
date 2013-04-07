3af9996b2fde5007d15101d8c844ea2437f0db6b
OpenShift guest <amtamt@gmail.com>
2013-04-07 10:43:22 -0400

    commit

--- nodepad/server.js
+++ nodepad/server.js
@@ -5,15 +5,11 @@ function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-        
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        var content = route(handle, pathname)
-        response.write(content);
-        response.end();
+
+        route(handle, pathname, response);
     }
     
     http.createServer(onRequest).listen(process.env.PORT,process.env.IP);
-    
     console.log("Server has started.");
 }
 
