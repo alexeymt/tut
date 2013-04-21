f3280498e4eed7314cd42f0a5e4c6c53d9e827d9
Алексей <amtamt@gmail.com>
2013-04-21 04:46:16 -0400

    Added testing framework

--- node-demo/server.js
+++ node-demo/server.js
@@ -1,11 +1,6 @@
-var express = require('express')
-  , app = express()
+var app = require(__dirname + '/app')
   , port = process.env.PORT || 3000
   , host = process.env.IP;
-  
-app.get('/', function (req, res) {
-  res.send('Hello, World!')
-})
 
 app.listen(port, host, function () {
   console.log('Listening on port ', port)
