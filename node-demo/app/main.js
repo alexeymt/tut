a8a9aabda69975a7836d8849367a868c23bd582a
Алексей <amtamt@gmail.com>
2013-04-21 05:05:07 -0400

    Added home page

--- node-demo/app/main.js
+++ node-demo/app/main.js
@@ -1,8 +1,14 @@
 var express = require('express')
   , app = express()
-  
-app.get('/', function (req, res) {
-  res.send('Hello, World!')
-})
+  , pages = require(__dirname + '/../controllers/pages')
+
+// configuration settings 
+app.set('views', __dirname + '/../views')
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+
+// mount routes
+app.get('/', function (req, res) { res.redirect('home') })
+app.get('/home', pages.home)
 
 module.exports = app
